645da5df9cbe87ac50d73476776d45dcb5cad8b1
Irushi Gunawaradana <irushigunawardana333@gmail.com>
2024-05-11 21:42:56 +0530

    Design Insert,Update and View Dialog added dependencies

--- app/src/main/java/com/example/easytask/MainActivity.kt
+++ app/src/main/java/com/example/easytask/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.easytask
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.easytask.databinding.ActivityMainBinding
+import android.widget.ImageView
+import com.example.easytask.utils.setupDialog
 
 class MainActivity : AppCompatActivity() {
+
+    private val mainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val addTaskDialog: Dialog by lazy {
+        Dialog(this, R.style.DialogCustomTheme).apply {
+            setupDialog(R.layout.add_task_dialog)
+        }
+    }
+
+    private val updateTaskDialog: Dialog by lazy {
+        Dialog(this, R.style.DialogCustomTheme).apply {
+            setupDialog(R.layout.update_task_layout)
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(mainBinding.root)
+
+
+
+        // Add task start
+        val addCloseImg = addTaskDialog.findViewById<ImageView>(R.id.closeImg)
+        val updateCloseImg = updateTaskDialog.findViewById<ImageView>(R.id.closeImg)
+
+        addCloseImg.setOnClickListener { addTaskDialog.dismiss() }
+        updateCloseImg.setOnClickListener { updateTaskDialog.dismiss() }
+
+        mainBinding.addTaskFABtn.setOnClickListener {
+            addTaskDialog.show()
+        }
     }
 }
