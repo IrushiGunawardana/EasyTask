07dd3d282db89f956b9eb74184a29be6055d648a
Irushi Gunawaradana <irushigunawardana333@gmail.com>
2024-05-12 09:32:39 +0530

    Started reading from database

--- app/src/main/java/com/example/easytask/dao/TaskDao.kt
+++ app/src/main/java/com/example/easytask/dao/TaskDao.kt
@@ -2,12 +2,14 @@ package com.example.easytask.dao
 
 import androidx.room.*
 import com.example.easytask.models.Task
-
+import kotlinx.coroutines.flow.Flow
 
 
 @Dao
 interface TaskDao {
 
+     @Query("SELECT * FROM Task ORDER BY date DESC")
+    fun getTaskList() : Flow<List<Task>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTask(task: Task): Long
